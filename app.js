1dedd89c94156939eebc8b084a96a4dfef0b6ee0
l0ng7h0r <bounluangt@gmail.com>
2025-11-21 18:50:56 +0700

    Fix CORS

--- app.js
+++ app.js
@@ -7,18 +7,7 @@ const paymentRoutes = require('./routes/paymentRoute');
 const analyticsRoutes = require('./routes/analyticsRoute');
 const authRoutes = require('./routes/auth');
 
-// FIXED: Removed trailing slash and added multiple origins
-app.use(cors({
-    origin: [
-        'https://frontend-sandy-iota-83.vercel.app',
-        'http://localhost:3000',
-        'http://localhost:5173'
-    ],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
